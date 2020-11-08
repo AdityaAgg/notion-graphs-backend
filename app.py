83ba9653cc36ba5c7e73abdd91c141ba4d9812f0
AdityaAgg <adityaaggarwalz200@gmail.com>
2020-11-07 20:51:11 -0800

    allow accept and no cache

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def get_data_points(cv, x_property, y_property, size_property, title_property, s
 # routes
 
 @app.route('/line_graph')
-@cross_origin(supports_credentials=True, origins=["https://adityaagg.github.io"], allow_headers="Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token")
+@cross_origin(supports_credentials=True, origins=["https://adityaagg.github.io"], allow_headers="Accept,Cache,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token")
 def get_all_events_route():
     notion_client = None
     notion_cookie = request.cookies.get("token_v2")
@@ -128,7 +128,7 @@ def get_all_events_route():
 
 
 @app.errorhandler(InvalidUsage)
-@cross_origin(supports_credentials=True, origins=["https://adityaagg.github.io"], allow_headers="Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token")
+@cross_origin(supports_credentials=True, origins=["https://adityaagg.github.io"], allow_headers="Accept,Cache,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token")
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
